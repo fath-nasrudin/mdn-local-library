f7653382882d498e1f9ef13b58ffcb98193f5842
fath-nasrudin <fath.nasrudin.i@gmail.com>
2023-12-11 22:47:22 +0700

    Add virtual formatted date of birth and death

--- models/author.model.js
+++ models/author.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
 
 const { Schema } = mongoose;
 
@@ -25,5 +26,18 @@ authorSchema.virtual('url')
     return `catalog/author/${this._id}`;
   });
 
+authorSchema.virtual('date_of_birth_formatted')
+  .get(function () {
+    return this.date_of_birth ?
+      DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED)
+      : '?';
+  })
+
+authorSchema.virtual('date_of_death_formatted')
+  .get(function () {
+    return this.date_of_death ?
+      DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
+      : '?';
+  })
 
 module.exports = mongoose.model('Author', authorSchema);

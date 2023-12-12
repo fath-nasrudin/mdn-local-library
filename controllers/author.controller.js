3ca46888e489291f4c453a20b6b7accded7b0742
fath-nasrudin <fath.nasrudin.i@gmail.com>
2023-12-12 12:03:43 +0700

    Add create author page and the handlers

--- controllers/author.controller.js
+++ controllers/author.controller.js
@@ -1,6 +1,7 @@
 const Author = require('../models/author.model');
 const Book = require('../models/book.model');
 const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
 
 // display list of all authors
 exports.author_list = asyncHandler(async (req, res) => {
@@ -35,13 +36,58 @@ exports.author_detail = asyncHandler(async (req, res) => {
 
 // display author create form on GET
 exports.author_create_get = asyncHandler(async (req, res) => {
-  res.send('NOT IMPLEMENTED: Author create GET');
+  res.render('author_form', {
+    title: 'Create Author'
+  })
 });
 
 // Handle Author create on POST.
-exports.author_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author create POST");
-});
+exports.author_create_post = [
+  body('first_name')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('First name must be specified.')
+    .isAlphanumeric()
+    .withMessage('First name has non-alphanumeric characters.'),
+
+  body('family_name')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Family name must be specified.')
+    .isAlphanumeric()
+    .withMessage('Family name has non-alphanumeric characters.'),
+
+  body('date_of_birth', 'Invalid date of birth')
+    .optional({ values: 'falsy' })
+    .isISO8601()
+    .toDate(),
+
+  // Process request after validation and sanitization.
+  asyncHandler(async (req, res, next) => {
+    // handling validation error
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render('author_form', {
+        title: 'Create Author',
+        author: req.body,
+        errors: errors.array(),
+      })
+      return;
+    }
+
+    const author = new Author({
+      first_name: req.body.first_name,
+      family_name: req.body.family_name,
+      date_of_birth: req.body.date_of_birth,
+      date_of_death: req.body.date_of_death,
+    });
+
+    await author.save();
+    res.redirect(author.url);
+  }),
+];
 
 // Display Author delete form on GET.
 exports.author_delete_get = asyncHandler(async (req, res, next) => {

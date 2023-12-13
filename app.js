f012feb3166298138aefe5d2d060ba1621e7c0de
fath-nasrudin <fath.nasrudin.i@gmail.com>
2023-12-13 09:16:48 +0700

    Add compression and helmet for production

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const compression = require('compression');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,10 +13,11 @@ var app = express();
 
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
-const MONGODB_URI = 'mongodb+srv://fathnasrudin:Mnczm7dHfy0EE0Oe@cluster0.fbjuovh.mongodb.net/local_library?retryWrites=true&w=majority';
+const MONGODB_URI_DEV = 'mongodb+srv://fathnasrudin:Mnczm7dHfy0EE0Oe@cluster0.fbjuovh.mongodb.net/local_library?retryWrites=true&w=majority';
+const mongoDB = process.env.MONGODB_URI || MONGODB_URI_DEV;
 
 async function main() {
-  await mongoose.connect(MONGODB_URI);
+  await mongoose.connect(mongoDB);
 }
 
 main().catch(err => console.log(err));
@@ -24,10 +26,19 @@ main().catch(err => console.log(err));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Add helmet to the middleware chain.
+// Set CSP headers to allow our Bootstrap and Jquery to be served
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    "srcipt-src": ["self", "code.jquery.com", "cdn.jsdelivr.net"]
+  }
+}))
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression()); // Compress all routes
 app.use(express.static(path.join(__dirname, 'public')));
 
 // routes
